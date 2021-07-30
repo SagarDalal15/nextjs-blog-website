4b230e77367d8331d185f44879aeeee0d7ae1c91
Sagar Dalal <sagardalal112@gmail.com>
2021-07-30 20:33:30 +0530

    meta tags added

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,16 @@ class MyDocument extends Document {
     return (
       <Html lang="en" prefix="og: http://ogp.me/ns#">
         <Head>
-          <meta property="og:title" content="Startup Website" />
+          <meta property="og:title" content="My Blog Website" />
           <meta name="image" property="og:image" content="/mysite.png" />
           <meta name="author" content="Sagar Dalal" />
           <meta
             property="og:description"
-            content="Web site created using NextJS"
+            content="Website created using NextJS"
           />
           <meta
             property="og:url"
-            content="https://nextjs-startup-website.vercel.app"
+            content="https://nextjs-blog-website-two.vercel.app"
           />
         </Head>
         <body>
