5b9cc6b73a85dcbcdb7f6cc613fbbb6d4e8c109e
Sagar Dalal <sagardalal112@gmail.com>
2021-07-30 20:27:01 +0530

    meta tags added

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,20 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html lang="en" prefix="og: http://ogp.me/ns#">
+        <Head>
+          <meta property="og:title" content="Startup Website" />
+          <meta name="image" property="og:image" content="/mysite.png" />
+          <meta name="author" content="Sagar Dalal" />
+          <meta
+            property="og:description"
+            content="Web site created using NextJS"
+          />
+          <meta
+            property="og:url"
+            content="https://nextjs-startup-website.vercel.app"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
